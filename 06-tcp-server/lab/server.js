93cfee17b9501265fe5733569f4f49f9777ace8e
Ben Harris <ben@harrishills.com>
2018-10-23 10:04:15 -0700

    Adding updates

--- 06-tcp-server/lab/server.js
+++ 06-tcp-server/lab/server.js
@@ -38,12 +38,25 @@ server.on('connection', (socket) => {
     const command = data.toString().split(' ').shift().trim();
     
     // @dm <username> <message>
+    //! REWRITE
+    let target = userPool.findIndex(user => {
+      return user.nickname === message.split(' ').shift().trim();
+    });
+    console.log(target);
+    userPool.forEach(user => {
+      user.socket.write(`${client.nickname} says: ${message.toString().split(' ').splice(1).join(' ')}`);
+    });
 
     // @quit
+    //! REWRITE
+    console.log(client.nickname);
+    let index = userPool.findIndex(user => {
+      return user.nickname === client.nickname;
+    });
+    userPool.splice(index, 1);
 
-    // @ <username> <message>
-
-    // @
+    userPool = userPool.filter((user) => user.nickname !== client.nickname);
+    client.socket.end();
 
     // @ handler
     if(command.startsWith('@')) {
@@ -58,7 +71,9 @@ server.on('connection', (socket) => {
 
 });
 
-// Event Listeners
+//-------------------------------------
+//* Event Listeners
+//-------------------------------------
 ee.on('@all', (client, message) => {
   userPool.forEach(user => {
     user.socket.write(`${client.nickname}: ${message}`);
@@ -73,4 +88,3 @@ ee.on('@nickname', (client, string) => {
 ee.on('default', (client) => {
   client.socket.write('Please begin all commands with @\n');
 });
-
