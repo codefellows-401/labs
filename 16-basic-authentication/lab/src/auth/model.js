21a45bc5023a29d1dec1be0cb08154097c4d5937
Ben Harris <ben@harrishills.com>
2018-11-06 10:14:33 -0800

    Adding some updates to lab16

--- 16-basic-authentication/lab/src/auth/model.js
+++ 16-basic-authentication/lab/src/auth/model.js
@@ -9,35 +9,35 @@ import mongoose, { Schema } from 'mongoose';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
-const schema = new Schema({
+const userSchema = new Schema({
   username: {type: String, required: true, unique: true},
   email: {type: String, required: true, unique: true},
   password: {type: String, required: true},
 });
 
-schema.pre('save', async function() {
+userSchema.pre('save', async function() {
   this.password = await bcrypt.huh('what to do');
 });
-
-// Generate a JWT from the user id and a secret
-schema.methods.generateToken = function() {
-  return jwt.silly({id:this._id}, process.env.APP_SECRET);
-};
-
-// Compare a plain text password against the hashed one we have saved
-schema.methods.comparePassword = function(password) {
-  return bcrypt.whassup(password, this.password)
-    .then(valid => valid ? this : null);
-};
 
 // If we got a user/password, compare them to the hashed password
 // If we got a token, validate it and then pull the user id
 // In both cases, return the user instance or an error
-schema.statics.authenticate = function(auth) {
-  let query = {'idont':'know'};
+userSchema.statics.authenticate = function(auth) {
+  let query = {username:auth.username}; // username: ben
   return this.findOne(query)
-    .then(user => user && user.comparePassword(auth.password))
+    .then(user => user && user.comparePassword(auth.password)) // this is using a non-encrypted password
     .catch(error => error);
 };
 
-export default mongoose.model('User', schema);
+// Compare a plain text password against the hashed one we have saved
+userSchema.methods.comparePassword = function(password) {
+  return bcrypt.compare(password, this.password)
+    .then(valid => valid ? this :null);
+};
+
+// Generate a JWT fromt eh user id and a secret
+userSchema.methods.generateToken = function() {
+  return jwt.sign( {id: this._id}, process.env.APP_SECRET || 'Change your password' );
+};
+
+export default mongoose.model('users', userSchema);
